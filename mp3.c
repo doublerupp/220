7d81e1588026bf248e10688730782016ff565b33
abiest19 <alecbiesterfeld@gmail.com>
2016-09-23 20:19:51 -0500

    fixed it

--- mp3.c
+++ mp3.c
@@ -9,26 +9,28 @@ int main()
 	int N;
 
 	//Scan the inputs from the user.
-	printf("Please enter omega1 omega2 omegac");
 	scanf("%lf %lf %lf", &omega1, &omega2, &omegac);
 	
 	T = 3 * 2 * M_PI / omega1;      // Total time
     	N = 20 * T / (2 * M_PI / omega2);   // Total number of time steps
     	dt = T / N;             // Time step
 
-	int i;
+	int i; //loop counter
 	double Voutnew = 0, Voutcur = 0, Voutprev = 0;
 
 	//Write your code here!
-	for(i=0; i<N; i++)
+	//for loop to run through the calculation an N number of times
+	for(i=0; i<=N; i++)
 	{
+		//break the discretized code into pieces a, b, and c for simplcity 
 		double a = 1/((1/(sqrt(2)*dt*omegac))+(1/(dt*dt*omegac*omegac)));
 		double b =((2/(dt*dt*omegac*omegac))-1)*Voutcur;
-		double c = 1/(sqrt(2)*dt*omegac)-1/(dt*dt*omegac*omegac)*Voutprev;
-		Voutnew= a*(b+c+sin(omega1*i*dt)+.5*sin(omega2*i*dt));
+		double c = (((1/(sqrt(2)*dt*omegac))-(1/(dt*dt*omegac*omegac))))*Voutprev;
+		Voutnew= (a)*(((b+c+sin((omega1*i*dt))+(.5*sin((omega2*i*dt))))));
 
-
-	printf("%lf", Voutnew);
+	//print out the current voltage value
+	printf("%lf \n", Voutcur); 
+	// update voltages before returning to the top of the loop
 	Voutprev = Voutcur;
 	Voutcur = Voutnew;
 	}
@@ -36,3 +38,5 @@ int main()
 
 	return 0;
 }
+
+    
